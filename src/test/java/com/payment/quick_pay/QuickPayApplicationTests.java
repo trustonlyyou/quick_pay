8e679a83184196b464eee41a645b74798528a061
trushonlyyou <trushonlyyou@tnctec.co.kr>
2025-03-08 11:19:08 +0900

    project init

---
+++ src/test/java/com/payment/quick_pay/QuickPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.quick_pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
